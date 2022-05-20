418f922ea7032382bfd83fc0335d7394ce22b454
Jeongun Baek <blackcow@coingo.gg>
2022-05-20 09:33:14 +0900

    remove key parameter from TBondFactoryV1's create() method

--- contracts/TBond/TBondFactoryV1.sol
+++ contracts/TBond/TBondFactoryV1.sol
@@ -8,13 +8,14 @@ import {TBondFundManagerV1} from "./TBondFundManagerV1.sol";
 contract TBondFactoryV1 is Ownable {
     mapping (bytes32 => address) public tokens;
 
-    function create(address _registry, bytes32 key, string memory name, string memory symbol)
+    function create(address _registry, string memory name, string memory symbol)
         onlyOwner
         external
         returns(address tbondAddress)
     {
+        bytes32 key = getKey(_msgSender(), name, symbol);
         require(tokens[key] == address(0), "TBondFactoryV1:alredy exists");
-        
+
         tbondAddress = address(new TBondFundManagerV1(_registry, name, symbol));
         TBondFundManagerV1(tbondAddress).changeManager(_msgSender());
 
