503151a5722356af103b0a72f03b86f8c0e7d8ad
Jeongun Baek <blackcow@coingo.gg>
2022-05-20 09:34:42 +0900

    add external modifier to WETH's balanceOf() method

--- contracts/interfaces/IWETH.sol
+++ contracts/interfaces/IWETH.sol
@@ -6,6 +6,6 @@ interface IWETH {
     function deposit() external payable;
     function transfer(address to, uint value) external returns (bool);
     function withdraw(uint) external;
-    function balanceOf(address) external returns (uint);
+    function balanceOf(address) view external returns (uint);
     function approve(address, uint) external;
 }
