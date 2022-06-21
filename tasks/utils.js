b1ba4b98c010415dc6ebfaf3e0a90014f113c4cf
combab0 <shpark@enki.co.kr>
2022-06-22 02:56:36 +0900

    working on refactoring

--- tasks/utils.js
+++ tasks/utils.js
@@ -18,28 +18,23 @@ const utils = {
 	LAYER2:'0x42ccf0769e87cb2952634f607df1c7d62e0bbc52',
 	SeigManager:'0x710936500ac59e8551331871cbad3d33d5e0d909',
 	StakeRegistry:'0x4Fa71D6964a97c043CA3103407e1B3CD6b5Ab367',
-  start_impersonate:async addr => {
-		await hre.network.provider.request({
-			method: 'hardhat_impersonateAccount',
-			params: [addr]
-		})
-	},
-	stop_impersonate:async addr => {
-		await hre.network.provider.request({
-			method: 'hardhat_stopImpersonatingAccount',
-			params: [addr]
-		})
-	},
 	set:(k, v) => {
 		if (hre.config.tbond == undefined) hre.config.tbond = {}
 		hre.config.tbond[k] = v
 	},
 	get:k => hre.config.tbond[k],
-	log:msg => console.log(msg ? msg : ''),
 	key:n => ethers.utils.id(`TBOND-${n}`),
-	parseTon:v => ethers.utils.parseEther(typeof(v) == 'string' ? v : BigInt(v).toString()),
-	fromTon:v => ethers.utils.formatEther(typeof(v) == 'string' ? v : BigInt(v).toString())
+	log:msg => console.log(msg ? msg : ''),
+	hex:v => `0x${v.toHexString().match(/(0x[0]+)([a-fA-F0-9]*)/)[2]}`,
+	sum:(...args) => eval(args.map(Number.parseFloat).join('+')),
+	parseEth:v => ethers.utils.parseUnits(Number.parseInt(v).toString(), 18),
+	parseTon:v => ethers.utils.parseUnits(Number.parseInt(v).toString(), 18),
+	parsewTon:v => ethers.utils.parseUnits(Number.parseInt(v).toString(), 27),
+	fromTon:v => ethers.utils.formatUnits(ethers.BigNumber.from(v), 18),
+	fromwTon:v => ethers.utils.formatUnits(ethers.BigNumber.from(v), 27),
+	mining:async n => await hre.network.provider.send('hardhat_mine', [`0x${n.toString(16)}`]),
+	start_impersonate:async addr => await hre.network.provider.send('hardhat_impersonateAccount',[addr]),
+	stop_impersonate:async addr => await hre.network.provider.send('hardhat_stopImpersonatingAccount', [addr]),
 }
 
-const imports = () => Object.keys(utils).forEach(id => global[id] = utils[id])
-module.exports = { imports }
+module.exports = { imports:() => Object.keys(utils).forEach(id => global[id] = utils[id]) }
